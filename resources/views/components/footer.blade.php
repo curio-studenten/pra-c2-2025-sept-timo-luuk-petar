ce31c22d97870ff3ba260664d3258821d2735e28
Timo <dte0011@edu.curio.nl>
2025-10-09 11:36:57 +0200

    Ticket 6 gedaan
    Ticket 6 de footer info gemaakt en toegevoegd

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,27 @@
 <footer class="footer bg-dark text-white py-3">
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-4">
                 {{ __('misc.copyright') }}
+            </div>
+            <div class="col-md-3">
+                <h5>Over ons</h5>
+                <p>Wij bieden een uitgebreide collectie handleidingen voor verschillende merken en producten.</p>
+            </div>
+            <div class="col-md-3">
+                <h5>Contact gegevens</h5>
+                <p>Email: info@example.com</p>
+                <p>Telefoon: +31 123 456 789</p>
+                <p>Adres: Voorbeeldstraat 123, Amsterdam</p>
+            </div>
+            <div class="col-md-2">
+                <h5>Social links</h5>
+                <div class="social-icons">
+                    <a href="https://facebook.com" target="_blank" class="text-white me-2"><i class="fab fa-facebook-f"></i></a>
+                    <a href="https://twitter.com" target="_blank" class="text-white me-2"><i class="fab fa-twitter"></i></a>
+                    <a href="https://instagram.com" target="_blank" class="text-white me-2"><i class="fab fa-instagram"></i></a>
+                    <a href="https://linkedin.com" target="_blank" class="text-white"><i class="fab fa-linkedin-in"></i></a>
+                </div>
             </div>
         </div>
     </div>
